731a0dc95165c254e426df6648aa0a20a65b8a9b
bmukha <47007157+bmukha@users.noreply.github.com>
2020-10-24 21:47:22 +0300

    deleting works

--- script.js
+++ script.js
@@ -44,11 +44,28 @@ function addBookToLibrary(title, author, pages, isRead) {
 
 function exposeShelf() {
   shelf.innerHTML = "";
-  myLibrary.forEach((item) => {
+  myLibrary.forEach((item, index) => {
     let newEl = document.createElement("div");
-    newEl.textContent = item.info();
+    newEl.data = index;
+    newEl.innerHTML =
+      `<div class="cardElement" id="cardIndex" data="${index}">Index: ${index + 1}</div> <div class="cardElement" id="cardTitle">Title: ${item.title}</div> <div class="cardElement" id="cardAuthor">Author: ${item.author}</div> <div class="cardElement" id="cardPages">Pages: ${item.pages}</div> <div class="cardElement" id="cardRead">Status: ${item.isRead ? "is read" : "not read yet"}</div> <button id="deleteBtn">DELETE</button>`;
     shelf.appendChild(newEl);
   });
 }
+
+function deleteBook(index) {
+  myLibrary.splice(index, 1);
+  console.table(myLibrary);
+  exposeShelf();
+}
+
+shelf.addEventListener('click', (e) => {
+  console.log(e.target.parentNode.data );
+  e.stopPropagation();
+  if (e.target.parentNode.data !== undefined) {
+    deleteBook(e.target.parentNode.data);
+  }
+});
+
 
 exposeShelf();
