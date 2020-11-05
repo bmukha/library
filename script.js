47052473435eb6842f123fae81ff94f62d91cdc5
bmukha <47007157+bmukha@users.noreply.github.com>
2020-11-05 16:59:31 +0200

    Redone with es6 classes

--- script.js
+++ script.js
@@ -24,15 +24,19 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
-function Book(title, author, pages, isRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
-  this.info = () =>
-    `"${this.title}" by ${this.author}, ${this.pages} pages, ${
+class Book {
+  constructor(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
+
+  info() {
+    return `"${this.title}" by ${this.author}, ${this.pages} pages, ${
       this.isRead ? "is read" : "not read yet"
     }.`;
+  }
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
